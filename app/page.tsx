bdeac9411be56bad217525bc912a5e974aeef2f0
Shreya-b21 <shreyanshb423@gmail.com>
2025-06-04 01:47:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
+      <div className={styles.shape}>
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
